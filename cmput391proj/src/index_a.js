d07886bb7c1a1b3809de3b0ec0725175cd631421
jim <jim@jim-Lenovo-IdeaPad-Y580>
2013-04-04 21:26:14 -0600

    added new home page

--- cmput391proj/src/index_a.js
+++ cmput391proj/src/index_a.js
@@ -4,14 +4,14 @@
 function OnWeLoad()
 {
 
-	Img2={max:1,pos:0};
-	Img2[0]=new Image();Img2[0].src='cmput391001001.jpg';
-	Img2[1]=new Image();Img2[1].src='cmput391001002.jpg';
-	Img4={max:1,pos:0};
-	Img4[0]=new Image();Img4[0].src='cmput391001003.jpg';
-	Img4[1]=new Image();Img4[1].src='cmput391001004.jpg';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
-	IDP[1]=(V5)?'document.getElementById(\'e2\').src':(IE)?'e2.src':'document.e2.src';
-	IDP[2]=(V5)?'document.getElementById(\'e4\').src':(IE)?'e4.src':'document.e4.src';
+	Img5={max:1,pos:0};
+	Img5[0]=new Image();Img5[0].src='cmput391001001.jpg';
+	Img5[1]=new Image();Img5[1].src='cmput391001002.jpg';
+	Img7={max:1,pos:0};
+	Img7[0]=new Image();Img7[0].src='cmput391001003.jpg';
+	Img7[1]=new Image();Img7[1].src='cmput391001004.jpg';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
+	IDP[1]=(V5)?'document.getElementById(\'e5\').src':(IE)?'e5.src':'document.e5.src';
+	IDP[2]=(V5)?'document.getElementById(\'e7\').src':(IE)?'e7.src':'document.e7.src';
 	isOvr=1;
 }
 
