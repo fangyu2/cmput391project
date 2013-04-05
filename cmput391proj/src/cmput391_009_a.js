744924f18d3306e2440ef5300ad5f3a37d1f12c5
jim <jim@jim-Lenovo-IdeaPad-Y580>
2013-04-04 20:47:47 -0600

    test

--- cmput391proj/src/cmput391_009_a.js
+++ cmput391proj/src/cmput391_009_a.js
@@ -4,26 +4,26 @@
 function OnWeLoad()
 {
 
-	Img2={max:1,pos:0};
-	Img2[0]=new Image();Img2[0].src='cmput391009001.jpg';
-	Img2[1]=new Image();Img2[1].src='cmput391009002.jpg';
-	Img4={max:1,pos:0};
-	Img4[0]=new Image();Img4[0].src='cmput391009003.jpg';
-	Img4[1]=new Image();Img4[1].src='cmput391009004.jpg';
-	Img6={max:1,pos:0};
-	Img6[0]=new Image();Img6[0].src='cmput391009005.jpg';
-	Img6[1]=new Image();Img6[1].src='cmput391009006.jpg';
-	Img8={max:1,pos:0};
-	Img8[0]=new Image();Img8[0].src='cmput391009007.jpg';
-	Img8[1]=new Image();Img8[1].src='cmput391009008.jpg';
-	Img10={max:1,pos:0};
-	Img10[0]=new Image();Img10[0].src='cmput391009009.jpg';
-	Img10[1]=new Image();Img10[1].src='cmput391009010.jpg';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
-	IDP[1]=(V5)?'document.getElementById(\'e2\').src':(IE)?'e2.src':'document.e2.src';
-	IDP[2]=(V5)?'document.getElementById(\'e4\').src':(IE)?'e4.src':'document.e4.src';
-	IDP[3]=(V5)?'document.getElementById(\'e6\').src':(IE)?'e6.src':'document.e6.src';
-	IDP[4]=(V5)?'document.getElementById(\'e8\').src':(IE)?'e8.src':'document.e8.src';
-	IDP[5]=(V5)?'document.getElementById(\'e10\').src':(IE)?'e10.src':'document.e10.src';
+	Img3={max:1,pos:0};
+	Img3[0]=new Image();Img3[0].src='cmput391009001.jpg';
+	Img3[1]=new Image();Img3[1].src='cmput391009002.jpg';
+	Img5={max:1,pos:0};
+	Img5[0]=new Image();Img5[0].src='cmput391009003.jpg';
+	Img5[1]=new Image();Img5[1].src='cmput391009004.jpg';
+	Img7={max:1,pos:0};
+	Img7[0]=new Image();Img7[0].src='cmput391009005.jpg';
+	Img7[1]=new Image();Img7[1].src='cmput391009006.jpg';
+	Img9={max:1,pos:0};
+	Img9[0]=new Image();Img9[0].src='cmput391009007.jpg';
+	Img9[1]=new Image();Img9[1].src='cmput391009008.jpg';
+	Img11={max:1,pos:0};
+	Img11[0]=new Image();Img11[0].src='cmput391009009.jpg';
+	Img11[1]=new Image();Img11[1].src='cmput391009010.jpg';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
+	IDP[1]=(V5)?'document.getElementById(\'e3\').src':(IE)?'e3.src':'document.e3.src';
+	IDP[2]=(V5)?'document.getElementById(\'e5\').src':(IE)?'e5.src':'document.e5.src';
+	IDP[3]=(V5)?'document.getElementById(\'e7\').src':(IE)?'e7.src':'document.e7.src';
+	IDP[4]=(V5)?'document.getElementById(\'e9\').src':(IE)?'e9.src':'document.e9.src';
+	IDP[5]=(V5)?'document.getElementById(\'e11\').src':(IE)?'e11.src':'document.e11.src';
 	isOvr=1;
 }
 
